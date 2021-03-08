1066967a6e9bcc83de7e89ae43cf20585e6fd6db
SkilzMastr <64101555+SkilzMastr@users.noreply.github.com>
2021-03-08 17:55:31 -0500

    Added Prices.json

--- pages/api/changePrice/[itemId].ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-
-
-
-export default (req: NextApiRequest, res: NextApiResponse) => {
-    const { itemId} = req.query;
-    res.send(itemId)
-}
--- pages/api/prices.ts
+++ pages/api/prices.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import * as data from './prices.json'
 
-//Tpye for return Data
 type Data = {
     meat_sticks: string,
     pencils: string,
@@ -10,5 +10,5 @@ type Data = {
 }
 
 export default (req: NextApiRequest, res: NextApiResponse<Data>) => {
-    res.status(200).json({ meat_sticks: "$1.25", pencils: "$1.00", no_no_snow: "$100", sporks: "$.45", gaming_chair_desk:"COMING SOON!"})
+    res.status(200).json({ meat_sticks: data.meat_sticks, pencils: data.pencils, no_no_snow: data.no_no_snow, sporks: data.sporks, gaming_chair_desk: data.gaming_chair_desk})
 }
